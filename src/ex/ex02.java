7a93fee3520967ff9dcc495721a1ac78555f39e9
dsada <ewrwre>
2016-12-26 16:46:52 +0800

    week12-20161226-SamaLin

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,23 @@
 /*
  * Topic: 給定一字串，請將此字串中，每個字元的 ASCII 編碼數值進行加總，輸出其編碼數 值總和。舉例來說，若輸入之字串為 “abcdefghij” 共 10 個字元，其 ASCII 編碼依序為 “97 98 99 100 101 102 103 104 105 106” ，則編碼總和輸出為 1015 。
  * Date: 2016/12/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021058 蕭懋霖
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int a=scn.nextInt();
+		for(int j=0;j<a+1;j++){
+			int sum=0;
+			String c=scn.nextLine();
+			for(int i=0;i<c.length();i++){
+				sum=sum+(int)c.charAt(i);
+			}
+			if(j!=0){System.out.println(sum);}
+		}
 	}
 
 }
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,14 +2,45 @@
 /*
  * Topic: OX 遊戲是大家小時候的童年記憶，甚至在長大後，無聊時還是會玩一下呢 ! 但當你一個人時，是否也能讓電腦跟你玩呢 ?
  * Date: 2016/12/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021058 蕭懋霖
  */
-
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int data[] = new int[9];
+		for (int i = 0; i < 9; i++) {
+			data[i] = scn.nextInt();
+		}
+		int sum = 0;
+		for (int i = 0; i < 9; i++) {
+			if (data[i] == 0) {
+				sum = sum + 1;
+			}
+		}
+		if (sum != 4 && sum != 5) {
+			System.out.println("False");
+		} else if (data[0] == data[1] && data[1] == data[2]) {
+			System.out.println("True");
+		} else if (data[3] == data[4] && data[4] == data[5]) {
+			System.out.println("True");
+		} else if (data[6] == data[7] && data[7] == data[8]) {
+			System.out.println("True");
+		} else if (data[0] == data[3] && data[3] == data[6]) {
+			System.out.println("True");
+		} else if (data[1] == data[4] && data[4] == data[7]) {
+			System.out.println("True");
+		} else if (data[2] == data[5] && data[5] == data[8]) {
+			System.out.println("True");
+		} else if (data[0] == data[4] && data[4] == data[8]) {
+			System.out.println("True");
+		} else if (data[2] == data[4] && data[4] == data[6]) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
 	}
 
 }
